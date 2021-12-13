920d28d6aeab3ee6860ad154fbccb21ce0e2b7c9
angela-araujo <angela.araujo.rj@gmail.com>
2021-12-13 20:46:46 -0300

    Finalizando autenticação: criação de middleware e rotas de apoio

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,24 +1,37 @@
 import { Router } from 'express';
 import * as userController from '../controllers/user.controller';
 import * as sessionController from '../controllers/session.controller';
+import * as movieController from '../controllers/movie.controller';
+import { authorize } from '../middlewares/auth';
 
 const apiRouter = Router();
 
 /* ROTAS GERAIS */
+
 apiRouter.get('/', (req, res) => {
     return res.json({
         message: 'Nossa primeira rota da API'
     });
 });
 
+
+
 /* ROTAS DE USUÁRIO */
+
 apiRouter.get('/users/id/:id', userController.view);
 apiRouter.post('/users/destroy/:id', userController.destroy);
 
+
 /* ROTAS DE SESSAO */
+
+apiRouter.get('/session/new', authorize, sessionController.index);
 apiRouter.post('/session/new', sessionController.create);
 
+
 /* ROTAS DE FILME */
+
+apiRouter.get('/movies', authorize, movieController.index);
+
 
 /* ROTAS DE LIST */
 
